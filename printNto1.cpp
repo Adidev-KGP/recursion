e2e1763c2394d9298e7cf5fa36bce63787b9205b
Adidev-KGP <74253561+Adidev-KGP@users.noreply.github.com>
2022-06-09 13:37:48 +0530

    Create printNto1.cpp

---
+++ printNto1.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(int i,int n){
+    if(i==0)return;
+    cout<<i;
+    solve(i-1,n);
+}
+
+signed main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);//FASTIO
+    int n;cin>>n;
+    solve(n,n);
+    
+    //int _t;cin>>_t;while(_t--)  //to run solve() t no. of times where t is no. of testcases
+    
+}
