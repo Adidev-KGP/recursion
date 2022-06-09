9bff29a962bf96dde5f437c0ee3ec90b87379e45
Adidev-KGP <74253561+Adidev-KGP@users.noreply.github.com>
2022-06-09 14:31:52 +0530

    Create check_pallindrom_.cpp

---
+++ check_pallindrom_.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool solve(string s,int n,int i){
+    if(i>=n/2)return true;
+    if(s[i]!=s[n-i-1])return false;
+    
+    return solve(s,n,i+1);
+}
+
+signed main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);//FASTIO
+    string s;
+    cin>>s;
+    int n=s.length();
+    cout<<solve(s,n,0);
+    
+    
+    //int _t;cin>>_t;while(_t--)  //to run solve() t no. of times where t is no. of testcases
+    
+}
