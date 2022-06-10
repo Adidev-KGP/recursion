e56e22f3123dee29077d50dc677e36beb6e051b1
Rajesh Kumar Singh <81872302+NutNick31@users.noreply.github.com>
2022-06-10 22:40:31 +0530

    Update check_pallindrom_.cpp

--- check_pallindrom_.cpp
+++ check_pallindrom_.cpp
@@ -5,14 +5,14 @@ bool solve(string s,int n,int i){
     if(i>=n/2)return true;
     if(s[i]!=s[n-i-1])return false;
     
-    return solve(s,n,i+1);
+    return solve(s,n,i+1);//this line looks good
 }
 
 signed main(){
     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);//FASTIO
     string s;
     cin>>s;
-    int n=s.length();
+    int n=s.length();/this line too
     cout<<solve(s,n,0);
     
     
